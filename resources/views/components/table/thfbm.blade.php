8a3dbff39a586b1fdea071328c4b68080f5b17c1
Bart Roos <bart@studiokaa.co>
2023-11-16 17:32:28 +0100

    Bugfix

--- resources/views/components/table/thfbm.blade.php
+++ resources/views/components/table/thfbm.blade.php
@@ -4,13 +4,17 @@
     'currentWeek',
     'feedbackmoment'
 ])
-<!-- <x-table.th zebra="{{ $loop->parent->parent->even }}" active="{{ $fbmsActive->pluck('id')->contains($feedbackmoment->id)}}" overdue="{{ $feedbackmoment->week < $currentWeek }}">
-    {{ $feedbackmoment->points }}
-</x-table.th> -->
 
-<th {{ $attributes->class([
-    'border px-1 py-1 md:py-2',
-    'bg-gray-100' => $loop->parent->parent->even,
-    'text-gray-300' => !$fbmsActive->pluck('id')->contains($feedbackmoment->id),
-    'text-red-400' => (!$fbmsActive->pluck('id')->contains($feedbackmoment->id) && ($feedbackmoment->week < $currentWeek))
-]) }}>{{ $slot }}</th>
+@if(isset($fbmsActive))
+    <th {{ $attributes->class([
+        'border px-1 py-1 md:py-2',
+        'bg-gray-100' => $loop->parent->parent->even,
+        'text-gray-300' => !$fbmsActive->pluck('id')->contains($feedbackmoment->id),
+        'text-red-400' => (!$fbmsActive->pluck('id')->contains($feedbackmoment->id) && ($feedbackmoment->week < $currentWeek)),
+    ]) }}>{{ $slot }}</th>
+@else
+    <th {{ $attributes->class([
+        'border px-1 py-1 md:py-2',
+        'bg-gray-100' => $loop->parent->parent->even,
+    ]) }}>{{ $slot }}</th>
+@endif
