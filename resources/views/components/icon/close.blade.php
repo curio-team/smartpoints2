bd38fb37193bd960fa5dd1ceb4ec3d7d678fea80
luttje <2738114+luttje@users.noreply.github.com>
2023-12-22 18:53:58 +0100

    Fix loading icon not spinning and icons not getting attributes

--- resources/views/components/icon/close.blade.php
+++ resources/views/components/icon/close.blade.php
@@ -1,4 +1,3 @@
-@props(['compact' => false])
-<x-icon.base :compact="$compact">
+<x-icon.base {{ $attributes }}>
     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 </x-icon.base>
